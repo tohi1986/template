bd8d41ba6964078bca13155fd9aee29dcfc806b8
Gagi <tohi1986@gmail.com>
2026-06-16 02:27:13 +0200

    ts updated - everything ready to use template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header.jsx"
-import Home from "./pages/Home.jsx";
+import Header from "./components/Header"
+import Home from "./pages/Home";
 
 function App() {
   return (
